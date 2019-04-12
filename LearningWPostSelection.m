6a1a4672a3f6c56aaf27477058f18b9ccb0fcf59
Omar Amer <omar.amer@uconn.edu>
2019-04-12 13:09:33 -0400

    Added PostSelection operator and Learning w post selectino function. Reasonably certain postselection is correct at least up to the operator itself, learningwpostselection needs to be tested

---
+++ LearningWPostSelection.m
@@ -0,0 +1,8 @@
+function HypothesisState = LearningWPostSelection(k, Es, rho)
+    Op = eye(2)
+    for idx = [1,k]
+        M = PostSelection(rho, Es{idx})
+        Op = M * Op
+    end
+    HypothesisState = Op * rho
+end
---
+++ PostSelection.m
@@ -0,0 +1,30 @@
+function PostSelectOp = PostSelection(st, E)
+    Z = {[1;0], [0;1]};
+    U = PostSelectionU(E);
+    PI = kron(eye(2), Z{1}*Z{1}');
+    UPU = U'*PI*U; 
+    inner = UPU * kron(st, Z{1}*Z{1}') * UPU;
+    PostSelectOp = (1/trace(E*st)) * PartialTrace(inner);
+end
+
+function PostUOp = PostSelectionU(MO)
+    Z = {[1;0], [0;1]};
+    PostUOp = zeros(4);
+    for idx = [1,2]
+        PostUOp = PostUOp + kron(SqrtMat(MO),eye(2)) * kron(Z{idx},Z{1}) * kron(Z{idx},Z{1})';
+        PostUOp = PostUOp + kron(SqrtMat(eye(2) - MO), eye(2)) * kron(Z{idx},Z{2}) * kron(Z{idx},Z{1})';
+        PostUOp = PostUOp + kron(Z{idx}, Z{2}) * kron(Z{idx}, Z{2})';
+    end
+end
+
+function SqrtOp = SqrtMat(Operator)
+    [V,D,W] = eig(Operator);
+    es = eig(Operator);
+    SqrtOp = [0,0;0,0];
+    for idx = (1:numel(es))
+        vec = V(:, idx);
+        mat = vec * vec';
+        SqrtOp = SqrtOp + sqrt(es(idx))*mat;
+    end
+end
+
