0c61787e77b1116931c3f1d6650e4f3f0fb55668
Hasan Iqbal <hasan.iqbal.anik@gmail.com>
2019-04-27 17:26:34 -0400

    delete extra file

--- regret_bound.m
+++ regret_bound.m
@@ -1,3 +1,18 @@
+% Regret minimization using Regularized follow the leader algorithm
+
+% define regret: sum of all losses from prediction minus sum of all losses
+% when the true state was the worst for us
+% r_t = sum(loss(prediction)) - minimum psi from C_n (sum(loss(measurement * psi)))
+
+% this loss function must be convex and L-Lipschitz
+
+% derivative of absolute value function is -1 if x < 0 or +1 if x > 0,
+% undefined if x = 0
+
+% Cn is the set of all trace-1 positive semi-definite complex matrices of
+% dimension 2^n
+
+
 [Es, bs, rho, L] = generate_dt_regret(100);
 
 
